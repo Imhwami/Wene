73ba9b1b8223b86d03bf9c41240370dd5a3e49fa
Imhwami <c30105210008@aeu.edu.my>
2024-06-12 21:57:29 +0700

    Revise the bug <Hr> tab in the all of main tab

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,17 @@
-import React, { useContext, useRef, useState } from 'react'
-import './Navbar.css'
-import logo from '../Assets/logo_2.png'
-import cart_icon from '../Assets/cart_icon.png'
-import { Link } from 'react-router-dom'
-import { ShopContext } from '../../Context/ShopContext'
-import nav_dropdown from '../Assets/nav_icon.png'
+import React, { useContext, useRef, useState } from 'react';
+import './Navbar.css';
+import logo from '../Assets/logo_2.png';
+import cart_icon from '../Assets/cart_icon.png';
+import { Link, useLocation } from 'react-router-dom';
+import { ShopContext } from '../../Context/ShopContext';
+import nav_dropdown from '../Assets/nav_icon.png';
 import { Tooltip } from 'react-tooltip';
 
 const Navbar = () => {
-    const [menu, setMenu] = useState("shop");
     const { getTotalCartItems } = useContext(ShopContext);
     const menuRef = useRef();
+    const location = useLocation();
+    const currentPath = location.pathname;
 
     const dropdown_toggle = (e) => {
         menuRef.current.classList.toggle('nav-menu-visible');
@@ -25,24 +26,41 @@ const Navbar = () => {
             </div>
             <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
             <ul ref={menuRef} className="nav-menu">
-                <li onClick={() => { setMenu("home") }}><Link style={{ textDecoration: 'none' }} to='/'>Home</Link>{menu === "home" ? <hr /> : <></>}</li>
-                <li onClick={() => { setMenu("wig") }}><Link style={{ textDecoration: 'none' }} to='/wig'>Wig</Link>{menu === "wig" ? <hr /> : <></>}</li>
-                <li onClick={() => { setMenu("eyelash") }}><Link style={{ textDecoration: 'none' }} to='eyelash'>Eyelash</Link>{menu === "eyelash" ? <hr /> : <></>}</li>
-                <li onClick={() => { setMenu("nails") }}><Link style={{ textDecoration: 'none' }} to='/nails'>Nails</Link>{menu === "nails" ? <hr /> : <></>}</li>
-                <li onClick={() => { setMenu("eyebrow") }}><Link style={{ textDecoration: 'none' }} to='/eyebrow'>Eyebrow</Link>{menu === "eyebrow" ? <hr /> : <></>}</li>
+                <li>
+                    <Link style={{ textDecoration: 'none' }} to='/'>Home</Link>
+                    {currentPath === "/" && <hr />}
+                </li>
+                <li>
+                    <Link style={{ textDecoration: 'none' }} to='/wig'>Wig</Link>
+                    {currentPath === "/wig" && <hr />}
+                </li>
+                <li>
+                    <Link style={{ textDecoration: 'none' }} to='/eyelash'>Eyelash</Link>
+                    {currentPath === "/eyelash" && <hr />}
+                </li>
+                <li>
+                    <Link style={{ textDecoration: 'none' }} to='/nails'>Nails</Link>
+                    {currentPath === "/nails" && <hr />}
+                </li>
+                <li>
+                    <Link style={{ textDecoration: 'none' }} to='/eyebrow'>Eyebrow</Link>
+                    {currentPath === "/eyebrow" && <hr />}
+                </li>
             </ul>
             <div className="nav-login-cart">
-                {localStorage.getItem('auth-token') ? <button onClick={() => { localStorage.removeItem('auth-token'); window.location.replace('/login') }}>Logout</button>
-                    : <Link to='/login'><button>Login</button></Link>
-                }
+                {localStorage.getItem('auth-token') ? (
+                    <button onClick={() => { localStorage.removeItem('auth-token'); window.location.replace('/login') }}>Logout</button>
+                ) : (
+                    <Link to='/login'><button>Login</button></Link>
+                )}
                 <Link to='/cart'><img src={cart_icon} alt="" /></Link>
                 <div data-tooltip-id="tooltip" data-tooltip-content="Total of our product on your cart" className="nav-cart-count">
-                {getTotalCartItems()}
-            </div>
-            <Tooltip id="tooltip" />
+                    {getTotalCartItems()}
+                </div>
+                <Tooltip id="tooltip" />
             </div>
         </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
