2d8c8f5b347bc524608ff03880360fb0d5f199c7
Imhwami <c30105210008@aeu.edu.my>
2024-08-07 22:14:13 +0700

    Fix the responsiveness for cart page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,7 +43,7 @@ function App() {
     
     <div>
       <BrowserRouter>
-      {/* <Navbar/> */}
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Shop/>}/>
         <Route path='/wig' element={<ShopCategory banner={wig_banner} category="wig"/>}/>
@@ -58,7 +58,7 @@ function App() {
         <Route path='/successfull' element={<Successfull/>}/>
         <Route path='/address' element={<Address/>}/>
       </Routes>
-      {/* <Footer/> */}
+      <Footer/>
       </BrowserRouter>
       <ToastContainer />
     </div>
